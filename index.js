4f2ab88b3ba48c3d8ee0e48b4001c7ed715f74ec
Christian Robards <christianrobards42@gmail.com>
2018-02-24 04:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ function sayHiToGrandma(string){
     return("I can't hear you!");
   else if (string == shout(string))
     return("YES INDEED!");
+  else(string == "I love you, Grandma.")
+    return("I love you, too.");
   
 }
